fc86a38829f473dfadfa7d84b691fd3de581577f
qouteall <qouteall@163.com>
2021-05-03 19:28:22 +0800

    initialize the mod

---
+++ src/main/java/qouteall/mini_scaled/MiniScaledModInitializer.java
@@ -0,0 +1,11 @@
+package qouteall.mini_scaled;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MiniScaledModInitializer implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        
+        System.out.println("MiniScaled Mod Initializing");
+    }
+}
