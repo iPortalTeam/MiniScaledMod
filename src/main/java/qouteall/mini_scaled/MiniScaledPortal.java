e6d439eea9c4c643a7078a9c18121cadf971e17a
qouteall <qouteall@163.com>
2021-05-07 16:23:28 +0800

    add collision adjustment

--- src/main/java/qouteall/mini_scaled/MiniScaledPortal.java
+++ src/main/java/qouteall/mini_scaled/MiniScaledPortal.java
@@ -1,5 +1,6 @@
 package qouteall.mini_scaled;
 
+import com.qouteall.immersive_portals.Helper;
 import com.qouteall.immersive_portals.McHelper;
 import com.qouteall.immersive_portals.portal.Portal;
 import net.fabricmc.api.EnvType;
@@ -14,12 +15,15 @@ import net.minecraft.entity.SpawnGroup;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.World;
 
 import java.util.function.Consumer;
 import java.util.function.Supplier;
+import java.util.stream.Stream;
 
 public class MiniScaledPortal extends Portal {
     public static EntityType<MiniScaledPortal> entityType;
@@ -35,7 +39,10 @@ public class MiniScaledPortal extends Portal {
     public void tick() {
         super.tick();
         
-        if (!world.isClient()) {
+        if (world.isClient()) {
+            tickClient();
+        }
+        else {
             if (world.getTime() % 3 == 0) {
                 checkValidity();
             }
@@ -126,42 +133,80 @@ public class MiniScaledPortal extends Portal {
     
     @Environment(EnvType.CLIENT)
     private void onCollidingWithEntityClientOnly(Entity entity) {
-        if (isOuterPortal() && !(getNormal().y < 0.9)) {
+        if (isOuterPortal() && (getNormal().y > 0.9)) {
             if (entity instanceof ClientPlayerEntity) {
+                
                 ClientPlayerEntity player = (ClientPlayerEntity) entity;
                 if (player.getPose() == EntityPose.CROUCHING) {
                     player.setPos(player.getX(), player.getY() - 0.01, player.getZ());
                     McHelper.updateBoundingBox(player);
+                }
+                else {
+                    levitatePlayer(player);
                 }
                 
                 Vec3d velocity = entity.getVelocity();
                 if (velocity.y < 0) {
                     entity.setVelocity(new Vec3d(velocity.x, velocity.y * 0.4, velocity.z));
                 }
-//                else {
-//                    entity.setVelocity(new Vec3d(velocity.x, velocity.y + 0.1, velocity.z));
-//                }
             }
         }
-        
-//        if (!isOuterPortal() && getNormal().y < -0.9) {
-//            Vec3d velocity = entity.getVelocity();
-//            if (velocity.y > 0) {
-//                entity.setVelocity(velocity.x, velocity.y +0.5, velocity.z);
-//            }
-//        }
+
     }
     
     @Override
     public void transformVelocity(Entity entity) {
         super.transformVelocity(entity);
-        
-//        if (isOuterPortal() && getNormal().y > 0.9) {
-//            entity.setVelocity(entity.getVelocity().multiply(0.5));
-//        }
+
+        if (isOuterPortal() && getNormal().y > 0.9) {
+            entity.setVelocity(entity.getVelocity().multiply(0.5));
+        }
 
 //        if (!isOuterPortal() && getNormal().y < -0.9) {
 //            entity.setVelocity(entity.getVelocity().add(0, 0.2, 0));
+//        }
+    }
+    
+    @Environment(EnvType.CLIENT)
+    private void tickClient() {
+    
+    }
+    
+    private void levitatePlayer(ClientPlayerEntity player) {
+        Box playerBox = player.getBoundingBox();
+        Box bottomHalfBox = playerBox.shrink(0, playerBox.getYLength() / 2, 0);
+        Box transformedBox = Helper.transformBox(bottomHalfBox, this::transformPoint);
+        World destWorld = getDestinationWorld();
+        Stream<VoxelShape> collisions = destWorld.getBlockCollisions(null, transformedBox);
+        double maxCollisionY = collisions.mapToDouble(s -> s.getBoundingBox().maxY)
+            .max().orElse(player.getY());
+        
+        Vec3d transformedPlayerPos = transformPoint(player.getPos());
+        
+        if (transformedPlayerPos.y < maxCollisionY) {
+            Vec3d newPosOtherSide =
+                new Vec3d(transformedPlayerPos.getX(), maxCollisionY, transformedPlayerPos.getZ());
+            Vec3d newPosThisSide = inverseTransformPoint(newPosOtherSide);
+            
+            player.updatePosition(player.getX(), newPosThisSide.y, player.getZ());
+        }
+
+//        World oldWorld = player.world;
+//        Vec3d oldPos = player.getPos();
+//        Box oldBB = player.getBoundingBox();
+//
+//        player.world = getDestinationWorld();
+//        Vec3d newPos = transformPoint(oldPos);
+//        player.setPos(newPos.x, newPos.y, newPos.z);
+//        player.setBoundingBox(Helper.transformBox(oldBB, this::transformPoint));
+//
+//        try {
+//
+//        }
+//        finally {
+//            player.world = oldWorld;
+//            player.setPos(oldPos.x, oldPos.y, oldPos.z);
+//            player.setBoundingBox(oldBB);
 //        }
     }
 }
